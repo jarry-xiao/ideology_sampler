4482320d117e1ad76e56ee94f692924e1aa76832
Jarry Xiao <jarry.xiao@gmail.com>
2020-06-01 17:48:51 -0500

    added quadrant sampling (wip)

--- ideology_sampler.py
+++ ideology_sampler.py
@@ -4,11 +4,65 @@ import random
 
 from IPython.display import display
 import psycopg2
+import numpy as np
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 from utils.db import df_to_postgres
+
+estimated_coefs = pd.read_csv("configs/estimated_coefs.csv")
+
+social = {
+    "libleft": lambda data: data.social < 0,
+    "libright": lambda data: data.social < 0,
+    "authleft": lambda data: data.social > 0,
+    "authright": lambda data: data.social > 0,
+}
+
+economic = {
+    "libleft": lambda data: data.economic < 0,
+    "libright": lambda data: data.economic > 0,
+    "authleft": lambda data: data.economic < 0,
+    "authright": lambda data: data.economic > 0,
+}
+
+
+def quadrant_sample(category, max_level=False):
+    answers = []
+    for i, data in estimated_coefs.iterrows():
+        if max_level:
+            magnitude = 1.5 
+        else:
+            magnitude = random.choice([.5, 1.5])
+        if data.economic != 0:
+            if economic[category](data):
+                answers.append(magnitude)
+            else:
+                answers.append(-magnitude)
+        if data.social != 0:
+            if social[category](data):
+                answers.append(magnitude)
+            else:
+                answers.append(-magnitude)
+    answers = (np.array(answers) + 1.5).astype(int).tolist()
+    print(answers)
+    return answers
+
+
+def standard_sampler(mode):
+    print(f"Mode: {mode}")
+    distributions = {"normal": [0, 1, 2, 3], "extreme": [0, 3]}
+    def f(q_id):
+        return random.choice(distributions[mode])
+    return f
+
+
+def answer_key(answers):
+    def f(q_id):
+        return answers[q_id]
+    return f
+
 
 class IdeologySampler:
 
@@ -55,20 +109,21 @@ class IdeologySampler:
                 print(f"Sample {i}: restarting driver")
                 self.restart_driver()
             try:
-                self.sample(self.start + i)
+                modes = ["normal", "extreme"]
+                mode = modes[round(random.random())]
+                f = standard_sampler(mode)
+                self.sample(self.start + i, mode, f)
                 i += 1
             except KeyboardInterrupt:
                 break
-            except:
+            except Exception as e:
+                print(e)
                 print(f"Encountered error on trial {i}, retrying...")
                 continue
 
-    def sample(self, t_id):
+    def sample(self, t_id, mode, f):
         self.reset()
-        modes = ["normal", "extreme"]
-        mode = modes[round(random.random())]
-        distributions = {"normal": [0, 1, 2, 3], "extreme": [0, 3]}
-        print(f"Loading data for trial {t_id}, mode: {mode}")
+        print(f"Loading data for trial {t_id}")
         q_data = []
         data = []
         q_id = 0
@@ -87,7 +142,7 @@ class IdeologySampler:
             for i, (prompt, question) in enumerate(zip(prompts, questions)):
                 options = prompt.find_elements_by_xpath(".//label")
                 q_text = question.text
-                value = random.choice(distributions[mode])
+                value = f(q_id) 
                 choice = options[value].find_elements_by_xpath(".//input")[0]
                 name = choice.get_attribute("name")
                 data.append([t_id, q_id, value])
@@ -117,13 +172,38 @@ class IdeologySampler:
             q_df = pd.DataFrame(q_data, columns=["q_id", "q_name", "q_text"])
             df_to_postgres(q_df, "questions", self.conn)
 
+    def quadrant(self, mode, max_level=False):
+        i = 0
+        while i < self.N:
+            # Hack to prevent excessive memory usage by Firefox
+            if i % 11 == 10:
+                print(f"Sample {i}: restarting driver")
+                self.restart_driver()
+            try:
+                answers = quadrant_sample(mode, max_level=max_level)
+                f = answer_key(answers)
+                self.sample(self.start + i, mode, f)
+                i += 1
+            except KeyboardInterrupt:
+                break
+            except Exception as e:
+                print(e)
+                print(f"Encountered error on trial {i}, retrying...")
+                continue
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--N")
+    parser.add_argument("--mode")
+    parser.add_argument("--max_level", action="store_true", default=False)
     args = parser.parse_args()
     if args.N is not None:
         ideology_sampler = IdeologySampler(int(args.N))
     else:
         ideology_sampler = IdeologySampler()
-    ideology_sampler()
+    if args.mode is not None:
+        print(args.max_level)
+        ideology_sampler.quadrant(args.mode, args.max_level)
+    else:
+        ideology_sampler()
